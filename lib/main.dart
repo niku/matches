9df51d651e29464fbd79d62681a3071c5491ab8b
niku <10890+niku@users.noreply.github.com>
2024-05-29 09:23:31 +0900

    Add prefix to prevent name collision of `Row`

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:http/http.dart' as http;
 import 'package:latlong2/latlong.dart';
 import 'package:matches/match_event.dart';
-import 'package:sqlite3/wasm.dart';
+import 'package:sqlite3/wasm.dart' as sqlite;
 
-Future<WasmSqlite3> _sqlite3 = WasmSqlite3.loadFromUrl(
+Future<sqlite.WasmSqlite3> _sqlite3 = sqlite.WasmSqlite3.loadFromUrl(
     Uri.parse('sqlite3.wasm')); // assume existing web/sqlite3.wasm
 
 final MapController _mapController = MapController();
@@ -39,18 +39,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late Future<CommonDatabase> db;
+  late Future<sqlite.CommonDatabase> db;
 
-  void _loadDatabase(final WasmSqlite3 sqlite3, final String path,
+  void _loadDatabase(final sqlite.WasmSqlite3 sqlite3, final String path,
       final Uint8List databaseBinary) {
-    final fs = InMemoryFileSystem();
+    final fs = sqlite.InMemoryFileSystem();
     sqlite3.registerVirtualFileSystem(fs, makeDefault: true);
 
     // borrowed by
     // https://github.com/tekartik/sqflite/blob/v2.3.0/packages_web/sqflite_common_ffi_web/lib/src/database_file_system_web.dart#L72-L88
     final file = fs
-        .xOpen(Sqlite3Filename(fs.xFullPathName(path)),
-            SqlFlag.SQLITE_OPEN_READWRITE | SqlFlag.SQLITE_OPEN_CREATE)
+        .xOpen(
+            sqlite.Sqlite3Filename(fs.xFullPathName(path)),
+            sqlite.SqlFlag.SQLITE_OPEN_READWRITE |
+                sqlite.SqlFlag.SQLITE_OPEN_CREATE)
         .file;
     try {
       file.xTruncate(0);
@@ -81,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Matches'),
       ),
       body: Center(
-        child: FutureBuilder<CommonDatabase>(
+        child: FutureBuilder<sqlite.CommonDatabase>(
           future: db,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
@@ -107,7 +109,7 @@ class MyMap extends StatefulWidget {
   final double defaultZoom = 6;
   final defaultMaxBounds =
       LatLngBounds(const LatLng(20.0, 122.0), const LatLng(50.0, 154.0));
-  final CommonDatabase database;
+  final sqlite.CommonDatabase database;
 
   @override
   State<StatefulWidget> createState() {
