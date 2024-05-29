2100a4e79e1821413e7e60a33e1a21c50ef574b1
niku <10890+niku@users.noreply.github.com>
2024-05-29 09:24:50 +0900

    Remove setting no longer needed

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Matches'),
       ),
       body: Center(
